7a47c5f2af4238291a37b000099b5feddc706c80
Chenjiu0111 <a154896863@qq.com>
2025-04-11 18:34:27 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//打印一个三行五列的*
+//效果如下：
+//*****
+//*****
+//*****
+
+
+#include<stdio.h>
+int main()
+{
+	for (int i = 1;i<=3;i++)
+	{
+		for (int j = 1;j <=5;j++)
+		{
+			printf("*");
+			
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
